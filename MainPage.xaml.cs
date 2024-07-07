df5df197e9730cce755c78cd6fe79a473af73b81
Yusuf Nviiri <yusufnviiri@gmail.com>
2024-07-07 04:57:38 +0300

    add api calls

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,25 +1,39 @@
-﻿namespace MauiAppMovies
+﻿using MauiAppMovies.Models;
+using System.Collections.ObjectModel;
+using System.Net.Http.Json;
+
+namespace MauiAppMovies
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        string apiKey = "63ff67fb44d6b922e7b92ac62c6188b1";
+        string movieUri = "https://api.themoviedb.org/3/";
+        private TrendingMovies movieList;
+        private GenreList genres;
+        public ObservableCollection<Genre> Genres { get; set; } = new();
+        public ObservableCollection<MovieResult> Movies { get; set; } = new();
+        public bool IsLoading { get; set; }
+        private readonly HttpClient httpClient;
 
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = this;
+            httpClient = new HttpClient { BaseAddress=new Uri(movieUri)};
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        protected override async void OnAppearing()
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            base.OnAppearing();
+            IsLoading = true;
+            OnPropertyChanged(nameof(IsLoading));
+            genres = await httpClient.GetFromJsonAsync<GenreList>($"genre/movie/list?api_key={apiKey}&language=en-US");
+            movieList = await httpClient.GetFromJsonAsync<TrendingMovies>($"trending/movie/week?api_key={apiKey}&language=en-US");
+            IsLoading = false;
+            OnPropertyChanged(nameof(IsLoading));
         }
+
+     
     }
 
 }
